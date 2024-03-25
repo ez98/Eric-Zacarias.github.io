854fe6f07a0df840fe21e7b379ef74f2e9e1b402
ezh98 <ericzacarias80@gmail.com>
2024-03-24 23:20:28 -0500

    testing script

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 API_TOKEN=$1
-echo "My secret in Bash script: $API_TOKEN"
+
+URL="https://github.com/ez98/Eric-Zacarias.github.io"
+HEADER1="Accept: application/vnd.github+json"
+HEADER2="Authorization: Bearer $API_TOKEN"
+I
+curl --request GET --url "$URL" --header "$HEADER1" --header "$HEADER2"
+
