7314cc86c1e2e19455b2c4ac12a1a4de1ef92cd2
ezh98 <ericzacarias80@gmail.com>
2024-03-24 23:28:32 -0500

    testing script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 API_TOKEN=$1
 
-URL="https://api.github.com/repos/OWNER/REPO/commits"
+URL="https://api.github.com/repos/ez98/Eric-Zacarias.github.io/commits"
 HEADER1="Accept: application/vnd.github+json"
 HEADER2="Authorization: Bearer $API_TOKEN"
 HEADER3="X-GitHub-Api-Version: 2022-11-28"
