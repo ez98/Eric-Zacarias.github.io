85a3ce85edf0af0696585f90d54880624d2f59a7
ezh98 <ericzacarias80@gmail.com>
2024-03-25 22:18:00 -0500

    new script changes

--- test.sh
+++ test.sh
@@ -13,8 +13,15 @@ COMMIT=`echo $RESPONSE | jq '.[0].commit'`
 NAME=`echo $COMMIT | jq '.author.name'`
 EMAIL=`echo $COMMIT | jq '.author.email'`
 DATE=`echo $COMMIT | jq '.author.date'`
+DATE=`echo $DATE | jq 'strptime("%Y-%m-%dT%H:%M:%SZ") | strftime("%Y-%m-%d")'`
 MESSAGE=`echo $COMMIT | jq '.message'`
+
 COMMIT_API_URL=`echo $COMMIT | jq -r '.url'`
 COMMIT_CURL_RESPONSE=$(curl -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $COMMIT_API_URL)
-echo $COMMIT_CURL_RESPONSE  
+
+COMMIT_CHANGES_URL=`echo $COMMIT_CURL_RESPONSE | jq '.html_url'`
+
+MARKDOWN="| {$NAME} | ${EMAIL} | ${DATE} | ${MESSAGE} | ${COMMIT_CHANGES_URL} |"
+
+echo $MARKDOWN
 # test new agent with jq installed
