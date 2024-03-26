3895ab000d26b888c4536a2f942c4835654f90af
ezh98 <ericzacarias80@gmail.com>
2024-03-25 21:23:48 -0500

    updated CURl statement

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ HEADER1="Accept: application/vnd.github+json"
 HEADER2="Authorization: Bearer $API_TOKEN"
 HEADER3="X-GitHub-Api-Version: 2022-11-28"
 
-curl --silent -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL
-
+RESPONSE=$(curl --silent -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL)
+jq --version
 # test new agent with jq installed
