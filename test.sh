f44a30feb7c21339a9fa4aae32a5cb1cfa2ebd9a
ezh98 <ericzacarias80@gmail.com>
2024-06-25 10:20:31 -0500

    code changes to commit_url

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ HEADER2="Authorization: Bearer $API_TOKEN"
 HEADER3="X-GitHub-Api-Version: 2022-11-28"
 
 RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL)
+echo $RESPONSE
 COMMIT_URL=$(echo $RESPONSE | jq -r '.[0].parents.[0].url')
 echo "THIS IS THE COMMIT_URL: ${COMMIT_URL}" 
 
