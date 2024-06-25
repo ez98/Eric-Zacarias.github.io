dfdb1061c5d38bf88711f4c852929b62e48b6cec
ezh98 <ericzacarias80@gmail.com>
2024-06-25 12:19:48 -0500

    testing curl

--- test.sh
+++ test.sh
@@ -8,16 +8,13 @@ HEADER2="Authorization: Bearer $API_TOKEN"
 HEADER3="X-GitHub-Api-Version: 2022-11-28"
 
 RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL)
-COMMITS_LIST=`echo $RESPONSE | jq '[.[].parents[].url]'`
+COMMITS_LIST=`echo $RESPONSE | jq '.[].parents[].url'`
 # LIST_LENGTH=`echo $COMMITS_LIST | jq 'length'`
 
 echo $COMMITS_LIST | while read -r commit; 
 do
     echo "do something with $commit"
 done
-
-first="${LST[1]}"
-echo "FIRST ELEMENT: $first"
 
 # for COMMIT in "${LST[@]}"; do
 #     echo $COMMIT
