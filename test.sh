9ee95a78461c83c718e2a4f3e334a14959b6d0a3
ezh98 <ericzacarias80@gmail.com>
2024-06-25 11:53:31 -0500

    testing curl

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ HEADER3="X-GitHub-Api-Version: 2022-11-28"
 
 RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL)
 COMMITS_LIST=$(echo $RESPONSE | jq '[.[].parents[].url]')
+declare -a $COMMITS_LIST
 
 for COMMIT in "${COMMITS_LIST[@]}"; do
     echo $COMMIT
