26b22a526e6319aadaa0fc223ed141b8724fca0d
ezh98 <ericzacarias80@gmail.com>
2024-06-25 10:29:14 -0500

    code changes to commit_url

--- test.sh
+++ test.sh
@@ -8,14 +8,13 @@ HEADER2="Authorization: Bearer $API_TOKEN"
 HEADER3="X-GitHub-Api-Version: 2022-11-28"
 
 RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL)
-echo $RESPONSE
 # COMMIT_URL=$(echo $RESPONSE | jq -r '.[0].parents.[0].url')
 COMMIT_URL=$(echo $RESPONSE | jq -r '.[0].parents.[0]')
 echo "THIS IS THE COMMIT_URL: ${COMMIT_URL}" 
 
 COMMIT_URL_RESPONSE=$(curl -s "https://api.github.com/repos/ez98/Eric-Zacarias.github.io/commits/0f41cf767b8220fb3fb96b1144377215ce068857")
 
-
+jq --version
 # COMMIT=`echo $RESPONSE | jq '.[0].commit'`
 
 # NAME=`echo $COMMIT | jq -r '.author.name'`
