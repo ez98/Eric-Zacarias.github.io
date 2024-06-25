cf0c10ac299aceffa0f9d5abc656e48b009c9556
ezh98 <ericzacarias80@gmail.com>
2024-06-25 09:18:05 -0500

    new updates to test script

--- test.sh
+++ test.sh
@@ -8,26 +8,32 @@ HEADER2="Authorization: Bearer $API_TOKEN"
 HEADER3="X-GitHub-Api-Version: 2022-11-28"
 
 RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $URL)
-COMMIT=`echo $RESPONSE | jq '.[0].commit'`
-
-NAME=`echo $COMMIT | jq -r '.author.name'`
-EMAIL=`echo $COMMIT | jq -r '.author.email'`
-DATE=`echo $COMMIT | jq '.author.date'`
-DATE=`echo $DATE | jq -r 'strptime("%Y-%m-%dT%H:%M:%SZ") | strftime("%Y-%m-%d")'`
-MESSAGE=`echo $COMMIT | jq -r '.message'`
-
-COMMIT_API_URL=`echo $COMMIT | jq -r '.url'`
-COMMIT_CURL_RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $COMMIT_API_URL)
-
-COMMIT_CHANGES_URL=`echo $COMMIT_CURL_RESPONSE | jq -r '.html_url'`
-
-MARKDOWN_LINE="| ${NAME} | ${EMAIL} | ${DATE} | ${MESSAGE} | [View Changes](${COMMIT_CHANGES_URL}) |"
+COMMIT_URL=`echo $RESPONSE | jq -r '.[0]."parents".[0].url'`
+echo "THIS IS THE COMMIT_URL: ${COMMIT_URL}" 
 
 
-echo -e $MARKDOWN_LINE >> /home/jenkins/workspace/github-test/README.md
 
-cat /home/jenkins/workspace/github-test/README.md
 
-git add README.md
-git commit -m "add readme"
-git push origin main
+# COMMIT=`echo $RESPONSE | jq '.[0].commit'`
+
+# NAME=`echo $COMMIT | jq -r '.author.name'`
+# EMAIL=`echo $COMMIT | jq -r '.author.email'`
+# DATE=`echo $COMMIT | jq '.author.date'`
+# DATE=`echo $DATE | jq -r 'strptime("%Y-%m-%dT%H:%M:%SZ") | strftime("%Y-%m-%d")'`
+# MESSAGE=`echo $COMMIT | jq -r '.message'`
+
+# COMMIT_API_URL=`echo $COMMIT | jq -r '.url'`
+# COMMIT_CURL_RESPONSE=$(curl -s -L -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" $COMMIT_API_URL)
+
+# COMMIT_CHANGES_URL=`echo $COMMIT_CURL_RESPONSE | jq -r '.html_url'`
+
+# MARKDOWN_LINE="| ${NAME} | ${EMAIL} | ${DATE} | ${MESSAGE} | [View Changes](${COMMIT_CHANGES_URL}) |"
+
+
+# echo -e $MARKDOWN_LINE >> /home/jenkins/workspace/github-test/README.md
+
+# cat /home/jenkins/workspace/github-test/README.md
+
+# git add README.md
+# git commit -m "add readme"
+# git push origin main
